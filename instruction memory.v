916caef537be956ed39b07f7ab46bd45d561e7c5
fadymedhat2236 <fadymedhat2236@gmail.com>
2017-11-23 21:17:03 +0200

    tested_version

--- instruction memory.v
+++ instruction memory.v
@@ -4,10 +4,10 @@ output [31:0] out;
 reg [31:0] IM [0:1023];//instruction memory is 4KB,address is 32 bit
 initial
 begin
-IM[0]=32 'h FFFFFFFF;
-IM[1]=1;
+IM[0]=32 'h 02508820; //  // add s1,s2,s0
+//IM[1]=32'h 02508822;  // sub s1,s2,s0
 
 end
-assign out=IM[read_address];
+assign out=IM[read_address>>2];
 
 endmodule
